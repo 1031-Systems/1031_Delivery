433467230ac0d85b1423b842fcbeadd5dab9f463
John Wright <steelheadj@adelphia.net>
2024-04-02 08:08:43 -0700

    Improved signaling

--- plugins/install.py
+++ plugins/install.py
@@ -1,19 +1,17 @@
 import Animatronics
 
 def invert(channelList, theanim):
+    retval = False  # Return False if NO channels are modified
     for channel in channelList:
         if ((channel.maxLimit > 1.0e33 and channel.minLimit > -1.0e33) or
             (channel.maxLimit < 1.0e33 and channel.minLimit < -1.0e33)): continue
-
+        retval = True   # Return True if ANY channel is modified
         for key in channel.knots:
             channel.knots[key] = (channel.maxLimit + channel.minLimit) - channel.knots[key]
 
-    return(True)
-
-def statistics(channelList):
-    return True
+    return(retval)
 
 channel_modifiers = [invert]
 channel_creators = []
-channel_analyzers = []
+channel_viewers = []
 
