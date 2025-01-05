c491a709dd5081f8d251c6b9eeefda89e6d2755c
John Wright <steelheadj@adelphia.net>
2025-01-04 18:36:55 -0800

    Moved Preferences into MainWindow code

--- MainWindow.py
+++ MainWindow.py
@@ -22,7 +22,31 @@ import importlib
 import pkgutil
 
 from Animatronics import *
-from Preferences import *
+
+# System Preferences Block
+SystemPreferences = {
+'MaxDigitalChannels':48,        # Maximum number of digital channels controller can handle
+'MaxServoChannels':32,          # Maximum number of servo/numeric channels controller cah handle
+'ServoDefaultMinimum':0,        # Default minimum servo setting
+'ServoDefaultMaximum':65535,    # Default maximum servo setting
+'AutoSave':True,                # Perfrom saving automatically flag
+'ShowTips':True,                # Show tool tips flag
+'ServoDataFile':'servotypes',   # Name of file containing predefined servos
+'UploadPath':'/sd/anims/',      # Name of upload directory on controller
+'TTYPortRoot':'/dev/ttyACM',    # Root of tty port for usb comm
+}
+SystemPreferenceTypes = {
+'MaxDigitalChannels':'int',
+'MaxServoChannels':'int',
+'ServoDefaultMinimum':'int',
+'ServoDefaultMaximum':'int',
+'AutoSave':'bool',
+'ShowTips':'bool',
+'ServoDataFile':'str',
+'UploadPath':'str',
+'TTYPortRoot':'str',
+}
+
 
 # Utilize XML to read/write animatronics files
 import xml.etree.ElementTree as ET
