e0629e476801a2a4bb08c138719deb923642b367
John Wright <steelheadj@verizon.net>
2025-04-17 16:38:07 -0700

    Updated imports for QAction moved to QtGui

--- Widgets.py
+++ Widgets.py
@@ -10,6 +10,7 @@ except:
     try:
         # PyQt6 import block for all widgets
         from PyQt6.QtCore import *
+        from PyQt6.QtGui import *
         from PyQt6.QtWidgets import *
         from PyQt6.QtPrintSupport import *
         usedPyQt = 6
