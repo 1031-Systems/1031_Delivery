947a442fe38eb2111565179fea9bc406df2cff2b
John Wright <steelheadj@adelphia.net>
2026-06-25 22:54:23 -0700

    Tweaks and additions to Windows batch files

--- install.bat
+++ install.bat
@@ -31,7 +31,7 @@ if errorlevel 1 (
     )
 )
 pip install -U PythonQwt
-pip install -U pygame
+pip install -U pygame-ce
 pip install -U rshell
 pip install -U pocketsphinx
 
---
+++ uninstall.bat
@@ -0,0 +1,20 @@
+@echo off
+:: No arguments
+:: Get path to this script and thus to the install directory
+set "SCRIPTPATH=%~dp0"
+:: Remove trailing backslash
+if "%SCRIPTPATH:~-1%"=="\" set "SCRIPTPATH=%SCRIPTPATH:~0,-1%"
+
+echo Uninstalling %SCRIPTPATH%
+
+:: Delete everything (move up one directory first, then delete)
+cd /d "%SCRIPTPATH%\.."
+rd /s /q "%SCRIPTPATH%"
+
+:: Remove Windows desktop shortcuts that point to this repo
+for %%f in ("%USERPROFILE%\Desktop\*.lnk") do (
+    findstr /m /i "%SCRIPTPATH%" "%%f" >nul 2>&1
+    if not errorlevel 1 (
+        del /f "%%f"
+    )
+)
